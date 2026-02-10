53051a6112efdee1973d9770d8619438d4fb36db
kayky-miranda <kayky.mm33@gmail.com>
2026-02-09 21:07:33 -0300

    chore: estrutura inicial

---
+++ src/main/java/com/contactsSystem/ContactsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.contactsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsSystemApplication.class, args);
+	}
+
+}
