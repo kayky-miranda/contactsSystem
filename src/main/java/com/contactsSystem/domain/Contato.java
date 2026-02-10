384d9c8eba152544da6dff3b291defa49c0c74c7
kayky-miranda <kayky.mm33@gmail.com>
2026-02-09 21:33:05 -0300

    feat: definindo a classe Endereco

--- src/main/java/com/contactsSystem/domain/Contato.java
+++ src/main/java/com/contactsSystem/domain/Contato.java
@@ -1,4 +1,33 @@
 package com.contactsSystem.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity(name="contatos")
+@Table(name="contatos")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
 public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @Column(unique = true) // definindo coluna email para receber valores unicos, evitando valores ja existentes
+    private String email;
+
+    @Column(unique = true) // definindo coluna telefone para receber valores unicos, evitando valores ja existentes
+    private String telefone;
+
+    private LocalDate dataNascimento;
+
 }
--- src/main/java/com/contactsSystem/domain/Endereco.java
+++ src/main/java/com/contactsSystem/domain/Endereco.java
@@ -1,4 +1,24 @@
 package com.contactsSystem.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua;
+    private String numero;
+    private String cep;
+
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+
 }
