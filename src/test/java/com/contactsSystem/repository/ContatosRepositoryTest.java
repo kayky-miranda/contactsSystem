a44f7999b9cf9c15c92275cf26385aeefb4ffa7b
kayky-miranda <kayky.mm33@gmail.com>
2026-03-18 19:46:24 -0300

    feat: testes unitários do repository concluido

--- src/test/java/com/contactsSystem/repository/ContatosRepositoryTest.java
+++ src/test/java/com/contactsSystem/repository/ContatosRepositoryTest.java
@@ -8,11 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest // classe de test para o JPA
 @ActiveProfiles("test") // utilizar o arquivo de teste
 class ContatosRepositoryTest {
+
+    @Autowired
+    ContatosRepository contatosRepository;
 
     @Autowired
     EntityManager entityManager;
@@ -20,17 +28,61 @@ class ContatosRepositoryTest {
     @Test
     @DisplayName("Teste unitário para verificar se o contato existe pelo e-mail")
     void existsByEmailCase1() {
-
+        String email = "test@gmail.com";
         Contato contato = new Contato(
                 null,
-                "Joao",
-                "test@gmail.com",
+                "username",
+                email,
                 "11999999999",
                 LocalDate.of(2000,1,10),
                 null
         );
         this.createContato(contato);
+
+        boolean result = this.contatosRepository.existsByEmail(email);
+
+        assertTrue(result);
     }
+
+    @Test
+    @DisplayName("Teste unitário para verificar se o contato não existe pelo e-mail")
+    void existsByEmailCase2() {
+        String email = "test@gmail.com";
+
+        boolean result = this.contatosRepository.existsByEmail(email);
+
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("Teste unitário para verificar se o contato existe pelo e-mail")
+    void existByTelefoneCase1(){
+
+        String telefone = "11999999999";
+        Contato contato = new Contato(
+                null,
+                "username",
+                "test@gmail.com",
+                telefone,
+                LocalDate.of(2000,1,10),
+                null
+        );
+        this.createContato(contato);
+
+        boolean result = this.contatosRepository.existsByTelefone(telefone);
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Teste unitário para verificar se o contato não existe pelo e-mail")
+    void existsByTelefoneCase2() {
+        String telefone = "11999999999";
+
+        boolean result = this.contatosRepository.existsByTelefone(telefone);
+
+        assertFalse(result);
+    }
+
 
     private Contato createContato(Contato contato){
         this.entityManager.persist(contato);
